9bafc058682cfed938f96c839dbaf0980bbcf4a9
gilles30 <lorison.gilles@gmail.com>
2022-12-01 00:00:33 -0300

    ✔️feat: created login page styles

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import { Button, Flex, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function SignIn() {
+  return (
+    <Flex w='100vw' h='100vh' align='center' justify='center'>
+      <Flex
+        as='form'
+        w='100%'
+        maxW={360}
+        bg='gray.800'
+        p={8}
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing={2}>
+          <Input type='email' name='email' label='E-mail' />
+          <Input type='password' name='password' label='Senha' />
+        </Stack>
+        <Button type='submit' mt='8' colorScheme='pink'>
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}

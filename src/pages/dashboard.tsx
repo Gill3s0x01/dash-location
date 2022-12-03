4899e2a7782374743ca7ee1c289139e7b5fabc61
gilles30 <lorison.gilles@gmail.com>
2022-12-03 19:15:44 -0300

    🔰feat: added responsive Daashboaard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -80,14 +80,14 @@ export default function Dashboard() {
           minChildWidth='320px'
           alignItems='flex-start'
         >
-          <Box p='8' bg='gray.800' borderRadius={8} pb='4'>
-            <Text fontSize='lg' mb='4'>
+          <Box p={['4', '8']} bg='gray.800' borderRadius={8} pb={['2', '4']}>
+            <Text fontSize='lg' mb={['2', '4']}>
               Locações da semana
             </Text>
             <Chart options={options} series={series} type='area' height={160} />
           </Box>
-          <Box p='8' bg='gray.800' borderRadius={8} pb='4'>
-            <Text fontSize='lg' mb='4'>
+          <Box p={['4', '8']} bg='gray.800' borderRadius={8} pb={['2', '4']}>
+            <Text fontSize='lg' mb={['2', '4']}>
               Taxa de locação
             </Text>
             <Chart options={options} series={series} type='area' height={160} />
